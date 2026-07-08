352d68dc5423576d49b5b1018386e84a5513a711
odon-airoldi <odon.airoldi@gmail.com>
2026-07-08 19:46:50 +0200

    Wrap projects view in app layout component

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,1 +1,14 @@
-Tutti i progetti
+<x-app-layout>
+
+    <div class="text-gray-800 dark:text-gray-200"> 
+        <h1>Tutti i progetti</h1>
+
+            @foreach ( $projects as $project )
+
+                {{ $project->name }}<br>
+                
+            @endforeach
+
+    </div>
+
+</x-app-layout>
