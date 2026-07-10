c34f2d941c9a50fc1ebd8d6bd87c9b951bd78a8d
odon-airoldi <odon.airoldi@gmail.com>
2026-07-10 15:59:53 +0200

    Fix indentation and formatting in projects view for improved readability

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,15 +1,18 @@
 <x-app-layout>
 
-    <div class="text-gray-800 dark:text-gray-200"> 
+    <div class="text-gray-800 dark:text-gray-200">
         <h1>Tutti i progetti</h1>
 
-            <ul>
+        <ul>
             @foreach ( $projects as $project )
 
-                <li><a href="{{ route('projects.show', $project->id) }}">{{ $project->name }}</a></li>
-                
+            <li><a href="{{ route('projects.show', $project->id) }}">{{ $project->name }}</a></li>
+
             @endforeach
-            </ul>
+        </ul>
+        <br>
+        <br>
+        <a href="{{ route('projects.create') }}">Aggiungi progetto</a>
 
     </div>
 
